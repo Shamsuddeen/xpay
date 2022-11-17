89a133112353f6cca2b380330b7350fca86968aa
Shamsuddeen <omacys2@gmail.com>
2022-11-17 19:11:53 +0100

    Redesign Wallet Creation

--- service/create-wallet.php
+++ service/create-wallet.php
@@ -3,30 +3,7 @@
 
     if ($_POST) {
         $currency   = trim($_POST['currency']);
-        $user       = $app->getUser(['id' => $userId]);
-        $key        = rand(001, 999).rand(001, 999).rand(001, 999).rand(1, 9);
-        $key        = md5($key.$user->uuid);
-        $url        = "/api/v2/virtual-accounts";
-        $data       = array(
-            "publicKey" => $_ENV['SEERBIT_PUB'],
-            "fullName" => $user->first_name." ".$user->last_name,
-            "bankVerificationNumber" => "",
-            "currency" => $currency,
-            "country" => $country,
-            "reference" => $key,
-            "email" => $user->email
-        );
-        $request    = $app->sendRequest('seerbit', 'POST', $url, ['body' => json_encode($data)], "");
-        $request    = json_decode($request, true);
-        if(strtolower($request['status']) == "success"){
-            $wallet = $app->createWallet(
-                $userId, 
-                $key, 
-                $request['data']['payments']['accountNumber'], 
-                $request['data']['payments']['bankName'], 
-                $currency, null
-            );
-
+            $wallet = $app->createWallet($userId, $currency, null);
             if($wallet == "success"){
 ?>
                 <script>
@@ -44,5 +21,5 @@
                 </script>
 <?php
             }
-        }
+
     }

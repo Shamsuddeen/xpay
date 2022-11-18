8eb0c23d612bac9fd3f0336e5bb74cf371b2f5b4
Shamsuddeen <omacys2@gmail.com>
2022-11-18 15:29:35 +0100

    Receive webhook notification

--- webhook.php
+++ webhook.php
@@ -1,3 +1,44 @@
+<?php
+    require('./app/app.class.php');
+    $response   = json_decode(file_get_contents('php://input'), true);
+    if($response['notificationItems'][0]['notificationRequestItem']['eventType'] == "transaction"){
+        $amount     = $response['notificationItems'][0]['notificationRequestItem']['data']['amount'];
+        $fee        = $response['notificationItems'][0]['notificationRequestItem']['data']['fee'];
+        $reference  = $response['notificationItems'][0]['notificationRequestItem']['data']['reference'];
+        if(array_key_exists('creditAccountNumber', $response['notificationItems'][0]['notificationRequestItem']['data'])){
+            $creditAccountNumber  = $response['notificationItems'][0]['notificationRequestItem']['data']['creditAccountNumber'];
+            $data = array(
+                'category' => 'credit',
+                'method' => "Virtual Account Transfer to $creditAccountNumber",
+                'amount' => $amount,
+                'fee' => $fee,
+            );
+            $wallet = $app->getWallet(['account_number', $creditAccountNumber]);
+        }
+        
+        if(strtolower($response['notificationItems'][0]['notificationRequestItem']['data']['description'] == "invoice link payment")){
+            $data = array(
+                'category' => 'credit',
+                'method' => "Invoice Payment to $creditAccountNumber",
+                'amount' => $amount,
+                'fee' => $fee,
+            );
+            $wallet = $app->getWallet(['account_number', $creditAccountNumber]);
+        }
+
+
+        // Fund Wallet
+        if($wallet != "404"){
+            $amount     -= $fee;
+            $oldBalance = $wallet->balance;
+            $newBalance = $oldBalance + $amount;
+
+            $fund = $app->fundWallet($wallet->id, $amount, $oldBalance, $newBalance, $data, $reference);
+        }
+    }
+
+?>
+
 <!-- {
     "notificationItems": [{
         "notificationRequestItem": {
@@ -31,8 +72,3 @@
         }
     }]
 } -->
-
-<?php
-    
-
-?>
